2a85aa6ba4bb3e75f03dde1fa8bef21537a0b6c0
deeheber <daheber@gmail.com>
2020-12-31 16:06:44 -0800

    Use fs/promises

--- update-readme.js
+++ update-readme.js
@@ -1,34 +1,34 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const Parser = require('rss-parser');
-const { promisify } = require('util');
-
-const readFile = promisify(fs.readFile);
-const writeFile = promisify(fs.writeFile);
 const parser = new Parser();
  
 (async () => {
-  // Get three latest posts from my blog
-  const feed = await parser.parseURL('https://www.danielleheberling.xyz/rss.xml');
+  try {
+    // Get three latest posts from my blog
+    const feed = await parser.parseURL('https://www.danielleheberling.xyz/rss.xml');
 
-  const postData = feed.items.map(post => {
-    const { title, link } = post;
-    return { title, link };
-  });
+    const postData = feed.items.map(post => {
+      const { title, link } = post;
+      return { title, link };
+    });
 
-  let updates = `<!-- start latest posts -->\n`;
-  for (let i = 0; i < 3; i++) {
-    const { link, title } = postData[i];
-    const row = `- [${title}](${link})\n`;
+    let updates = `<!-- start latest posts -->\n`;
+    for (let i = 0; i < 3; i++) {
+      const { link, title } = postData[i];
+      const row = `- [${title}](${link})\n`;
 
-    console.log(`Post #${i + 1} found. Title: ${title} Link: ${link}\n`);
-    updates = updates.concat(row);
+      console.log(`Post #${i + 1} found. Title: ${title} Link: ${link}\n`);
+      updates = updates.concat(row);
+    }
+    updates = updates.concat('<!-- end latest posts -->');
+
+    // Rewrite README with new post content
+    const currentText = await fs.readFile('README.md', 'utf8');
+    const postsSection = /<!-- start latest posts -->[\s\S]*<!-- end latest posts -->/g;
+    const newText = currentText.replace(postsSection, updates)
+
+    await fs.writeFile('README.md', newText);
+  } catch (error) {
+    console.error('there was an error:', error.message);
   }
-  updates = updates.concat('<!-- end latest posts -->');
-
-  // Rewrite README with new post content
-  const currentText = await readFile('README.md', 'utf8');
-  const postsSection = /<!-- start latest posts -->[\s\S]*<!-- end latest posts -->/g;
-  const newText = currentText.replace(postsSection, updates)
-
-  await writeFile('README.md', newText);
 })();
