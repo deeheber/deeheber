678715a88d0a997426cc4a72c6025cc60b8058e9
Danielle Heberling <deeheber@users.noreply.github.com>
2021-11-25 06:01:27 -0800

    Update code deps

--- update-readme.js
+++ update-readme.js
@@ -1,29 +1,27 @@
-const fs = require('fs/promises');
-const Parser = require('rss-parser');
+import fs from 'fs/promises';
+import Parser from 'rss-parser';
 const parser = new Parser();
  
-(async () => {
-  try {
-    // Get three latest posts from my blog's RSS feed
-    const { items } = await parser.parseURL('https://www.danielleheberling.xyz/rss.xml');
+try {
+  // Get three latest posts from my blog's RSS feed
+  const { items } = await parser.parseURL('https://www.danielleheberling.xyz/rss.xml');
 
-    let updates = `<!-- start latest posts -->\n`;
-    for (let i = 0; i < 3; i++) {
-      const { link, title } = items[i];
-      const row = `- [${title}](${link})\n`;
+  let updates = `<!-- start latest posts -->\n`;
+  for (let i = 0; i < 3; i++) {
+    const { link, title } = items[i];
+    const row = `- [${title}](${link})\n`;
 
-      console.log(`Post #${i + 1} found. Title: ${title} Link: ${link}\n`);
-      updates = updates.concat(row);
-    }
-    updates = updates.concat('<!-- end latest posts -->');
-
-    // Rewrite README with new post content
-    const currentText = await fs.readFile('README.md', 'utf8');
-    const postsSection = /<!-- start latest posts -->[\s\S]*<!-- end latest posts -->/g;
-    const newText = currentText.replace(postsSection, updates)
-
-    await fs.writeFile('README.md', newText);
-  } catch (error) {
-    console.error('there was an error:', error.message);
+    console.log(`Post #${i + 1} found. Title: ${title} Link: ${link}\n`);
+    updates = updates.concat(row);
   }
-})();
+  updates = updates.concat('<!-- end latest posts -->');
+
+  // Rewrite README with new post content
+  const currentText = await fs.readFile('README.md', 'utf8');
+  const postsSection = /<!-- start latest posts -->[\s\S]*<!-- end latest posts -->/g;
+  const newText = currentText.replace(postsSection, updates)
+
+  await fs.writeFile('README.md', newText);
+} catch (error) {
+  console.error('there was an error:', error.message);
+}
